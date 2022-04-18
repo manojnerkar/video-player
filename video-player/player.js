2a4cc1fdcdc09325bed5fcec3473a8335e847404
MANOJ NERKAR <79189614+manojnerkar@users.noreply.github.com>
2022-04-18 12:20:26 +0530

    Add files via upload

---
+++ video-player/player.js
@@ -0,0 +1,187 @@
+var playlistData = [
+  {
+    "id": "1",
+    "title": "Ed Sheeran - Shape of You (Official Music Video)",
+    "thumbnail": "https://img.youtube.com/vi/JGwWNGJdvx8/sddefault.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack",
+    "thumbnail": "https://img.youtube.com/vi/RgKAFK5djSk/sddefault.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Mark Ronson - Uptown Funk (Official Video) ft. Bruno Mars",
+    "thumbnail": "https://img.youtube.com/vi/OPf0YbXqDm0/sddefault.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Maroon 5 - Sugar (Official Music Video)",
+    "thumbnail": "https://img.youtube.com/vi/09R8_2nJtjg/sddefault.jpg"
+  },
+  {
+    "id": "5",
+    "title": "Justin Bieber - Sorry (PURPOSE : The Movement)",
+    "thumbnail": "https://img.youtube.com/vi/fRh_vgS2dFE/sddefault.jpg"
+  },
+  {
+    "id": "6",
+    "title": "Katy Perry - Roar (Official)",
+    "thumbnail": "https://img.youtube.com/vi/CevxZvSJLk8/sddefault.jpg"
+  },
+  {
+    "id": "7",
+    "title": "OneRepublic - Counting Stars (Official Music Video)",
+    "thumbnail": "https://img.youtube.com/vi/hT_nvWreIhg/sddefault.jpg"
+  },
+  {
+    "id": "8",
+    "title": "Ed Sheeran - Thinking Out Loud (Official Music Video)",
+    "thumbnail": "https://img.youtube.com/vi/lp-EO5I60KA/sddefault.jpg"
+  },
+  {
+    "id": "9",
+    "title": "Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)",
+    "thumbnail": "https://img.youtube.com/vi/aJOTlE1K90k/sddefault.jpg"
+  },
+  {
+    "id": "10",
+    "title": "Alan Walker - Faded",
+    "thumbnail": "https://img.youtube.com/vi/60ItHLz5WEA/sddefault.jpg"
+  }
+]
+
+
+var videoPlaySectionData = [
+  {
+    "id": "1",
+    "title": "Ed Sheeran - Shape of You (Official Music Video",
+    "description": "In 2019, Ed Sheeran released the genre-spanning ‘No.6 Collaborations Project’ which featured a wide-range of artists including ‘Justin Bieber’, ‘Camilla’ Cabello’, ‘Travis Scott’, ‘Eminem’, ‘Cardi B, ‘Paulo Londra’, ‘Bruno Mars’ and ‘Stormzy’ amongst many others, producing hits such as ‘I Don’t Care’, ‘Beautiful People’, ‘South of The Border’ and ‘Take Me Back To London’. ",
+    "views": 5682844633,
+    "youtubeID": "JGwWNGJdvx8",
+    "isLiked": "true",
+    "isSaved": "true"
+  },
+  {
+    "id": "2",
+    "title": "Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack",
+    "description": "Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack",
+    "views": 5474869150,
+    "youtubeID": "RgKAFK5djSk",
+    "isLiked": "false",
+    "isSaved": "false"
+  },
+  {
+    "id": "3",
+    "title": "Mark Ronson - Uptown Funk (Official Video) ft. Bruno Mars",
+    "description": "Official Video for Uptown Funk by Mark Ronson ft. Bruno Mars Listen to Mark Ronson: https://MarkRonson.lnk.to/listenYD",
+    "views": 4529335870,
+    "youtubeID": "OPf0YbXqDm0",
+    "isLiked": "false",
+    "isSaved": "true"
+  },
+  {
+    "id": "4",
+    "title": "Maroon 5 - Sugar (Official Music Video)",
+    "description": "Catch Maroon 5 on tour all year long  at www.maroon5.com Music video by Maroon 5 performing Sugar. (C) 2015 Interscope Records",
+    "views": 3679663909,
+    "youtubeID": "09R8_2nJtjg",
+    "isLiked": "false",
+    "isSaved": "true"
+  },
+  {
+    "id": "5",
+    "title": "Justin Bieber - Sorry (PURPOSE : The Movement",
+    "description": "Director: Parris Goebel Producer: Justin Bieber, Scott Scooter Braun and Parris Goebel Animator: John Hwang Production Company: SB Films and Taktix FilmsChoreographer: Parris GoebelDancers: The Ladies of ReQuest & The Royal Family Dance Crews from The Palace Dance Studio, NZ",
+    "views": 3533096553,
+    "youtubeID": "fRh_vgS2dFE",
+    "isLiked": true,
+    "isSaved": true
+  },
+  {
+    "id": "6",
+    "title": "Katy Perry - Roar (Official)",
+    "description": "Official music video for Katy Perry's Roar brought to you in Junglescope directed by Grady Hall & Mark Kudsi and produced by Javier Jimenez, Danny Lockwood, Patrick Nugent, Derek Johnson and Oualid Mouaness.",
+    "views": 3560669490,
+    "youtubeID": "CevxZvSJLk8",
+    "isLiked": true,
+    "isSaved": true
+  },
+  {
+    "id": "7",
+    "title": "OneRepublic - Counting Stars (Official Music Video)",
+    "description": "Music video by OneRepublic performing Counting Stars. (C) 2013 Mosley Music/Interscope Records",
+    "views": 3551240142,
+    "youtubeID": "hT_nvWreIhg",
+    "isLiked": true,
+    "isSaved": true
+  },
+  {
+    "id": "8",
+    "title": "Ed Sheeran - Thinking Out Loud (Official Music Video)",
+    "description": "In 2019, Ed Sheeran released the genre-spanning ‘No.6 Collaborations Project’ which featured a wide-range of artists including ‘Justin Bieber’, ‘Camilla’ Cabello’, ‘Travis Scott’, ‘Eminem’, ‘Cardi B, ‘Paulo Londra’, ‘Bruno Mars’ and ‘Stormzy’ amongst many others, producing hits such as ‘I Don’t Care’, ‘Beautiful People’, ‘South of The Border’ and ‘Take Me Back To London’. ",
+    "views": 3433535389,
+    "youtubeID": "lp-EO5I60KA",
+    "isLiked": true,
+    "isSaved": true
+  },
+  {
+    "id": "9",
+    "title": "Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)",
+    "description": "Music video by Maroon 5 performing Girls Like You. © 2018 Interscope Records",
+    "views": 3271316131,
+    "youtubeID": "aJOTlE1K90k",
+    "isLiked": true,
+    "isSaved": true
+  },
+  {
+    "id": "10",
+    "title": "Alan Walker - Faded",
+    "description": "The song Faded is a revamped version of Walker's past composition Fade, which was released via the record label NoCopyrightSounds in 2014. In the creation ",
+    "views": 3263101740,
+    "youtubeID": "60ItHLz5WEA",
+    "isLiked": true,
+    "isSaved": true
+  }
+]
+
+
+var playlistwraaper = document.getElementById("playlist-wrapper")
+var playerwrapper = document.getElementById("player-wrapper")
+
+for (var i = 0; i < playlistData.length; i++) {
+
+  playlistwraaper.innerHTML +=
+    `
+                   <div id="card${i + 1}" onclick ='renderVideo(${i + 1})' class="playlist-card ${i == 0 ? 'active-card' : ''}">
+                        <img class="thumbnail" src="${playlistData[i].thumbnail}" />
+                        <h3 class="video-card-title">${playlistData[i].title}</h3>
+                    </div>
+             `
+}
+
+
+
+function renderVideo(id) {
+  $('.playlist-card').removeClass("active-card");
+  $("#card"+id).addClass("active-card")
+  var data = videoPlaySectionData[id - 1];
+  playerwrapper.innerHTML =
+    `
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.youtubeID}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div>
+                        <div id="video-actions">
+                            <p><span id="views-count">${(data.views)}</span> views</p>
+
+                            <div>
+                                <i class="far fa-heart"></i>
+                                <i class="far fa-comment-alt"></i>
+                                <i class="far fa-bookmark"></i>
+                            </div>
+                        </div>
+                        <h3 id="video-title">${data.title}</h3>
+                        <p id="video-description">${data.description}</p>
+                    </div>
+         </div>           
+        `
+}
